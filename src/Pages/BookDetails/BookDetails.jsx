c1d0b8085909b32ed9ab6a0bfdecf7f33f20e072
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-03-27 02:45:09 +0600

    designed book details card

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useLoaderData, useParams } from 'react-router-dom';
+
+import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetails = () => {
-    const bookDetails = useLoaderData();
+  const bookDetails = useLoaderData();
 
-    const {bookId} = useParams();
-    const currentBookId = parseInt(bookId);
+  const { bookId } = useParams();
+  const currentBookId = parseInt(bookId);
 
-    const currentBook = bookDetails.find(book => book.bookId === currentBookId)
+  const currentBook = bookDetails.find((book) => book.bookId === currentBookId);
 
-    return (
-        <div>
-            <h2>Details {bookDetails.length}</h2>
+  const {
+    image,
+    bookName,
+    author,
+    category,
+    review,
+    tags,
+    totalPages,
+    publisher,
+    yearOfPublishing,
+    rating,
+  } = currentBook;
+
+  return (
+    <div className="card lg:card-side bg-base-100 shadow-xl mt-12">
+      <figure className="">
+        <img src={image} alt="book" className="w-1/2" />
+      </figure>
+      <div className="card-body">
+        <div className="border-b-2 pb-3">
+          <h2 className="text-4xl font-extrabold my-2">{bookName}</h2>
+          <h3 className="text-xl font-medium">By: {author}</h3>
         </div>
-    );
+
+        <h3 className="text-xl font-medium my-2">{category}</h3>
+
+        <div className="text-lg pt-3 border-t-2">
+          <p>
+            <span className="font-bold">Review: </span>
+            {review}
+          </p>
+          <div className="flex gap-5 items-center my-3">
+            <span className="font-bold">Tag</span>
+            {tags.map((tag, idx) => (
+              <h3
+                className="text-[#23BE0A] px-5 py-2 rounded-3xl bg-[#23BE0A0D] font-medium"
+                key={idx}
+              >
+                <span className="font-bold">#</span>
+                {tag}
+              </h3>
+            ))}
+          </div>
+        </div>
+
+        <div className="text-lg py-3 border-t-2 flex gap-6">
+          <div>
+            <h3>Number of Pages: </h3>
+            <h3 className="my-3">Publisher: </h3>
+            <h3>Year of Publishing: </h3>
+            <h3 className="my-3">Rating: </h3>
+          </div>
+          <div>
+            <h3 className="font-bold">{totalPages}</h3>
+            <h3 className="my-3 font-bold">{publisher}</h3>
+            <h3 className="font-bold">{yearOfPublishing}</h3>
+            <h3 className="my-3 font-bold">{rating}</h3>
+          </div>
+        </div>
+        
+        <div className="card-actions">
+          <button className="btn border-2 bg-transparent">Read</button>
+          <button className="btn bg-[#50B1C9] text-white">Wishlist</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-BookDetails.propTypes = {
-    
-};
+BookDetails.propTypes = {};
 
 export default BookDetails;
--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -12,7 +12,7 @@ const Book = ({ book }) => {
           <img src={image} alt="book" className="rounded-xl h-80 w-80" />
         </figure>
         <div className="card-body">
-          <div className="flex gap-6">
+          <div className="flex gap-5">
             {tags.map((tag, idx) => (
               <h3
                 className="text-[#23BE0A] px-4 py-2 rounded-3xl bg-[#23BE0A0D] font-medium"
@@ -23,8 +23,8 @@ const Book = ({ book }) => {
             ))}
           </div>
           <h2 className="text-2xl font-bold">{bookName}</h2>
-          <h3 className="text-lg">By: {author}</h3>
-          <div className="flex w-full justify-between pt-3 border-t-2 border-dashed text-lg">
+          <h3 className="text-lg font-medium">By: {author}</h3>
+          <div className="flex w-full justify-between pt-3 border-t-2 border-dashed text-lg font-medium">
             <h3>{category}</h3>
             <h3 className="flex items-center gap-1">
               {rating}
