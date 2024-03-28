ef1b510fbe9d60c004744f8da091c7b942fecd72
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-03-28 15:36:08 +0600

    added useRouteError hook in ErrorPage

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  const error = useRouteError();
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div>
-          <h1 className="text-5xl font-bold mb-6">404 page not found</h1>
-          <Link to="/"><button className="btn bg-[#23BE0A] text-white">Go Back</button></Link>
+          <h1 className="text-5xl font-bold mb-2">{error.status}</h1>
+          <h1 className="text-4xl font-bold mb-4">{error.statusText}</h1>
+          <p className="mb-6">{error.data}</p>
+          <Link to="/">
+            <button className="btn bg-[#23BE0A] text-white">Go Back</button>
+          </Link>
         </div>
       </div>
     </div>
